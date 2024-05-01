79df7c2cd8cc9c4a68c1d27e63622cc672486e88
rayui <49279418+Rayui1225@users.noreply.github.com>
2024-05-01 16:06:46 +0800

    Update Minimum Number of Arrows to Burst Balloons.cpp

--- Algorithm/Minimum Number of Arrows to Burst Balloons.cpp
+++ Algorithm/Minimum Number of Arrows to Burst Balloons.cpp
@@ -1,3 +1,8 @@
+/************************************************************
+*Reframe the problem as determining how many points overlap with a reference point (note that there may be multiple reference points).
+*To check if a point overlaps with a reference point, verify if Xstart is greater than the reference point. Consequently, sort the points vector to facilitate a single pass through it.
+*As you iterate through the vector, increment the result counter when a point does not overlap with the current reference point, then update the reference point to this new non-overlapping point.
+************************************************************/    
 class Solution {
 public:
     static bool cmp(vector<int> a,vector<int> b){
@@ -20,3 +25,7 @@ public:
         return res;
     }
 };
+/************************************************************
+somewhere can improve
+I can just compare a[0] < a[1] and if p[0] < tmp we just change tmp to  min(tmp,p[1])
+************************************************************/  
